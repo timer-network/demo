3df866a1d6a76aa6a882a63632d60960586aaba9
his <his@norepay.github.com>
2023-09-14 17:46:19 +0800

    w

---
+++ build/precache-manifest.8299c72a0adad1d20e1cfb35303fd0a3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e80a34deb94743291ec7f9531189fc15",
+    "url": "/index.html"
+  },
+  {
+    "revision": "408824dc574963cbc73e",
+    "url": "/static/css/2.3888b3e0.chunk.css"
+  },
+  {
+    "revision": "6fa7d7b5571dd262be2f",
+    "url": "/static/css/main.b4f5c8a2.chunk.css"
+  },
+  {
+    "revision": "408824dc574963cbc73e",
+    "url": "/static/js/2.15195ea1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.15195ea1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6fa7d7b5571dd262be2f",
+    "url": "/static/js/main.14204b7f.chunk.js"
+  },
+  {
+    "revision": "7802deec0234eb7ab7ff",
+    "url": "/static/js/runtime-main.6f67a2e3.js"
+  }
+]);
--- src/index.js
+++ src/index.js
@@ -4,3 +4,14 @@ import App from './App'
 import './assets/common.css';
 
 ReactDOM.render(<App />, document.getElementById('root'))
+
+baseFontSize()
+window.addEventListener('resize', baseFontSize)
+
+function baseFontSize (){
+  const base = 37.5 / 375
+
+  const shouldBeWide = base
+
+  document.documentElement.style.fontSize = shouldBeWide + 'px'
+}
